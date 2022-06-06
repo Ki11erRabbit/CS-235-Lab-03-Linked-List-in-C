c9e984f8261b0bbacb1e5ab9f208825d3a478f41
ki11errabbit <unlikelytitan@gmail.com>
2022-06-06 16:33:56 -0600

    began adding code for Linked List functions

--- LinkedList.c
+++ LinkedList.c
@@ -3,3 +3,128 @@
 //
 
 #include "LinkedList.h"
+
+Node* LinkedList_initializeNode(const char* data, Node* next, Node* previous) {
+    Node* tempNode;
+    tempNode = malloc(sizeof(Node));
+    *tempNode->data = *data;
+    tempNode->next = next;
+    tempNode->previous = previous;
+    return tempNode;
+}
+
+void LinkedList_clear(void) {
+    if (head->next != NULL) {
+        Node* currNode = head;
+        while (currNode != NULL) {
+            Node* nextNode = currNode->next;
+            free(currNode);
+            currNode = nextNode;
+        }
+        head = NULL;
+    }
+    else {
+        printf("There are no nodes");
+    }
+
+}
+
+void LinkedList_push_front(const char* value) {
+    head = LinkedList_initializeNode(value, head, NULL);
+}
+void LinkedList_push_back(const char* value) {
+    tail = LinkedList_initializeNode(value, NULL, tail);
+}
+
+void LinkedList_pop_front(void) {
+    if (head == NULL) {
+        printf("Empty Linked List!");
+        return;
+    }
+    Node* newHead = head->next;
+    free(head);
+    head = newHead;
+}
+void LinkedList_pop_back(void) {
+    if (tail == NULL) {
+        printf("Empty Linked List!");
+        return;
+    }
+    Node* newTail = tail->previous;
+    free(tail);
+    tail = newTail;
+}
+
+char* LinkedList_front(void) {
+    if (head == NULL) {
+        printf("Error! Empty Linked List!");
+        return strdup("Error");
+    } else {
+        return head->data;
+    }
+}
+char* LinkedList_back(void) {
+    if (tail == NULL) {
+        printf("Error! Empty Linked List!");
+        return strdup("Error");
+    } else {
+        return tail->data;
+    }
+}
+
+int LinkedList_is_empty(void) {
+    if (head == NULL && tail == NULL) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+void LinkedList_remove_value(const char* value) {
+    if (head == NULL || tail == NULL) {
+        printf("Error! Linked List Empty!");
+    }
+    else {
+        Node* currNode = head;
+        while (currNode->next != NULL) {
+            if (strcmp(currNode->data,value) && currNode == head) {
+                LinkedList_pop_front();
+                currNode = head;
+            }
+            else if (strcmp(currNode->next->data, value)) {
+                Node* nextNode = currNode->next->next;
+                Node* previousNode = currNode;
+                free(currNode->next);
+                previousNode->next = nextNode;
+                nextNode->previous = previousNode;
+            }
+            else {
+                currNode = currNode->next;
+            }
+        }
+        if (strcmp(head->data, value)) {
+            free(head);
+            head = NULL;
+        }
+    }
+}
+
+void LinkedList_reverse(void) {
+    Node* currNode = head;
+    if (head == NULL) {
+        printf("Error! Empty Linked List!");
+        return;
+    }
+
+}
+
+size_t LinkedList_size(void) {
+    Node* currNode = head;
+    size_t count = 0;
+    while (currNode != NULL) {
+        count++;
+        currNode = currNode->next;
+    }
+    return count;
+}
+char* LinkedList_toString(void);
--- LinkedList.h
+++ LinkedList.h
@@ -5,4 +5,34 @@
 #ifndef LAB_03_LINKED_LIST_LINKEDLIST_H
 #define LAB_03_LINKED_LIST_LINKEDLIST_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Node{
+    char* data;
+    struct Node* next;
+    struct Node* previous;
+} Node;
+
+Node* head = NULL;
+Node* tail = NULL;
+
+Node* LinkedList_initializeNode(const char* data, Node* next, Node* previous);
+void LinkedList_push_front(const char* value);
+void LinkedList_push_back(const char* value);
+void LinkedList_pop_front(void);
+void LinkedList_pop_back(void);
+char* LinkedList_front(void);
+char* LinkedList_back(void);
+int LinkedList_is_empty(void);
+void LinkedList_remove_value(const char* value);
+void LinkedList_clear(void);
+void LinkedList_reverse(void);
+size_t LinkedList_size(void);
+char* LinkedList_toString(void);
+
+
+
+
 #endif //LAB_03_LINKED_LIST_LINKEDLIST_H
